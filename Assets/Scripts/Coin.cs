14fa0c2006fa22e852752ceb2470b3f7127d1eee
Kagan Tek <kagantek2003@hotmail.com>
2024-01-30 12:59:49 +0300

    Collectibles added

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    [SerializeField] private AudioClip clickSound;
+    private int count = 0;
+ 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Collectible"))
+        {
+            count++;
+            AudioSource.PlayClipAtPoint(clickSound, collision.transform.position);
+            Destroy(collision.gameObject);
+        }
+    }
+}
